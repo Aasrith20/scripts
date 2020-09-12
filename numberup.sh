7af2ba33dd9fa3f9517a8bb769ab5d1f0e46a9cf
Aasrith20 <62925727+Aasrith20@users.noreply.github.com>
2020-09-12 10:47:46 +0530

    Create numberup.sh

---
+++ numberup.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+if [ "$1" == "--help" ]
+then 
+    echo "   
+          #################################################################################
+                This command is very much useful when you want to quickly rename files 
+                to your desired type in numerical sequence 1.jpg 2.jpg 3.jpg
+          #################################################################################"
+    echo "
+          SYNTAX : numberup --help/type path -v 
+
+          ~~ --help gives you this output 
+          ~~ type specifies the type of file you want to change to 
+          ~~ path is place where your files are present
+          ~~ -v gives you a description about what is going on 
+    "
+else
+cd $2
+count=1
+for i in *
+do 
+  if [ "$3" == "-v" ]
+  then 
+     echo "$i >> $count$1"
+  fi
+  mv "$i" "$count$1"
+  count=`expr $count + 1`
+done 
+fi
